b5665b9f44c60eae3c5a1092fb083b5da5e66385
piyushNegiDev <piyushnegi381@gmail.com>
2025-07-11 23:01:00 +0530

    Updated js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let a = parseInt(prompt("Enter first Number"));
+/*let a = parseInt(prompt("Enter first Number"));
 let b = parseInt(prompt("Enter second Number"));
 
 let random = Math.random();
@@ -37,4 +37,30 @@ if (operation === "+") {
   }
 } else {
   console.log("Wrong Operation Choice");
+}
+*/
+
+// Harry Solution
+
+let no1 = prompt("Enter first number");
+let no2 = prompt("Enter second number");
+let operation = prompt("Enter operation");
+
+let falseOp = {
+  "+": "-",
+  "*": "+",
+  "-": "/",
+  "/": "**",
+};
+
+let random = Math.random();
+
+if (random > 0.1) {
+  alert(`The result is ${eval(`${no1} ${operation} ${no2}`)}`);
+} else {
+  console.log(falseOp[operation]);
+  operation = falseOp[operation];
+  console.log(operation);
+
+  alert(`The result is ${eval(`${no1} ${operation} ${no2}`)}`);
 }
