496dce41ef52a12111e6ba77e95ec85d8226c577
piyushNegiDev <piyushnegi381@gmail.com>
2025-07-11 16:46:26 +0530

    Add Initial Files

---
+++ index.js
@@ -0,0 +1,38 @@
+let a = parseInt(prompt("Enter first Number"));
+let b = parseInt(prompt("Enter second Number"));
+
+let random = Math.random();
+console.log(random);
+
+// + -
+// * +
+// - /
+// / **
+
+let operation = prompt("Which operation you want to perform : + , - , * , /");
+
+if (operation === "+") {
+  if (random < 0.1) {
+    console.log("Wrong calculation : ", a - b);
+  } else {
+    console.log("Right calculation : ", a + b);
+  }
+} else if (operation === "-") {
+  if (random < 0.1) {
+    console.log("Wrong calculation : ", a / b);
+  } else {
+    console.log("Right calculation : ", a - b);
+  }
+} else if (operation === "*") {
+  if (random < 0.1) {
+    console.log("Wrong calculation : ", a + b);
+  } else {
+    console.log("Right calculation : ", a * b);
+  }
+} else if (operation === "/") {
+  if (random < 0.1) {
+    console.log("Wrong calculation : ", a ** b);
+  } else {
+    console.log("Right calculation : ", a / b);
+  }
+}
